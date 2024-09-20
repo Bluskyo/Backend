712c1d163fbcc4ad66177dfc5e0d9dbfb1f7615c
Bluskyfishing <blusky445@gmail.com>
2024-09-20 21:15:51 +0200

    innit

---
+++ src/main/java/github/io/bluskyfishing/Katsuyou/KatsuyouApplication.java
@@ -0,0 +1,13 @@
+package github.io.bluskyfishing.Katsuyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatsuyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatsuyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/bluskyfishing/Katsuyou/KatsuyouApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.bluskyfishing.Katsuyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatsuyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
