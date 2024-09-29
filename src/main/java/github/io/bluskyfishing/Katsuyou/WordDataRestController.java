436b2ead1a8a86f54a57b906b8e04ff731a48d8e
Bluskyfishing <blusky445@gmail.com>
2024-09-29 14:00:12 +0200

    added CrossOrigin tag so frontend can call.

--- src/main/java/github/io/bluskyfishing/Katsuyou/WordDataRestController.java
+++ src/main/java/github/io/bluskyfishing/Katsuyou/WordDataRestController.java
@@ -2,16 +2,18 @@ package github.io.bluskyfishing.Katsuyou;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WordDataRestController {
+    @CrossOrigin(origins = "http://127.0.0.1:5173")
     @GetMapping("/")
     public ResponseEntity<Word> getWordData(){
         return ResponseEntity.status(HttpStatus.OK).body(new WordDataService().word());
 
-        
+
     }
 
 }
